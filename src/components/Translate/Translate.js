6de12308c6df556260d8d772acfc3f4d4fa96e7f
arttuhar <arttuhar@gmail.com>
2022-01-31 13:13:51 +0200

    FEAT: Get translate input value and create image elements
    Co-authored-by: TimoJarvenpaa <32742872+TimoJarvenpaa@users.noreply.github.com>

--- src/components/Translate/Translate.js
+++ src/components/Translate/Translate.js
@@ -1,11 +1,72 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { translationCreateAction } from "../../store/actions/translationActions";
 import TranslateForm from "./TranslateForm";
 import TranslateResult from "./TranslateResult";
 
 const Translate = () => {
+	const [translation, setTranslation] = useState("");
+	const [translationResult, setTranslationResult] = useState("");
+	const [translatedSigns, setTranslatedSigns] = useState([]);
+
+	const dispatch = useDispatch();
+	const { id } = useSelector(state => state.session);
+	const { translations } = useSelector(state => state.translation);
+
+	const onInputChange = e => {
+		setTranslation(e.target.value);
+	};
+
+	const onTranslationSubmit = e => {
+		e.preventDefault();
+		setTranslatedSigns([]);
+		const translationObject = {
+			translation: translation,
+			deleted: "false",
+		};
+		const newTranslations = translations.concat(translationObject);
+		dispatch(
+			translationCreateAction({
+				translations: newTranslations,
+				id: id,
+			})
+		);
+		setTranslationResult(translation);
+		setTranslatedSigns(translateToSigns(translation));
+		setTranslation("");
+	};
+
+	const baseSrc = "/assets/individial_signs/";
+	const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
+
+	const translateToSigns = translation => {
+		const translationSigns = [];
+		const charArray = [...translation];
+		charArray.forEach((letter, index) => {
+			if (alphabet.includes(letter.toLowerCase())) {
+				translationSigns.push(
+					<img
+						src={baseSrc + letter + ".png"}
+						key={index + letter}
+						alt={letter}
+					/>
+				);
+			}
+		});
+		return translationSigns;
+	};
+
 	return (
 		<div className="Translate">
-			<TranslateForm />
-			<TranslateResult />
+			<TranslateForm
+				translation={translation}
+				onInputChange={onInputChange}
+				onTranslationSubmit={onTranslationSubmit}
+			/>
+			<TranslateResult
+				translation={translationResult}
+				translatedSigns={translatedSigns}
+			/>
 		</div>
 	);
 };
--- src/components/Translate/TranslateForm.js
+++ src/components/Translate/TranslateForm.js
@@ -1,40 +1,17 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { translationCreateAction } from "../../store/actions/translationActions";
 import AppContainer from "../../hoc/AppContainer";
 import "./TranslateForm.css";
 
-const TranslateForm = () => {
-	const [translation, setTranslation] = useState("");
-	const dispatch = useDispatch();
-	const { loggedIn, id } = useSelector(state => state.session);
-	const { translations } = useSelector(state => state.translation);
-
-	const onInputChange = e => {
-		setTranslation(e.target.value);
-	};
-
-	const onTranslationSubmit = e => {
-		e.preventDefault();
-		const translationObject = {
-			translation: translation,
-			deleted: 'false'
-		}
-		const newTranslations = translations.concat(translationObject);
-		dispatch(translationCreateAction({ 
-			translations: newTranslations,
-			id: id 
-		}));
-		setTranslation("");
-	};
+const TranslateForm = props => {
+	const { loggedIn } = useSelector(state => state.session);
 
 	return (
 		<div className="TranslateForm">
 			<AppContainer>
 				{!loggedIn && <Navigate to="/" />}
 				<form
-					onSubmit={onTranslationSubmit}
+					onSubmit={props.onTranslationSubmit}
 					className="translate-form mt-3 pt-1 px-4 pb-5 shadow p-3 mb-5"
 				>
 					<div className="input-group pt-4">
@@ -45,8 +22,8 @@ const TranslateForm = () => {
 							id="username"
 							type="text"
 							placeholder="Translate"
-							onChange={onInputChange}
-							value={translation}
+							onChange={props.onInputChange}
+							value={props.translation}
 							className="translate-input form-control border-start-0 border-end-0 shadow-none"
 							aria-describedby="translate-button"
 						/>
